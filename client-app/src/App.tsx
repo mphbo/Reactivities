f3b6fb894b98474ed189d4382b94dea5852f4d7a
loganSSW <logan@synicsw.com>
2021-11-14 16:58:57 -0800

    added semantic ui styling, called api on clientside in useEffect, as well as added cors to backend

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/activities')
+      .then(response => {
+        console.log(response.data);
+        setActivities(response.data);
+      })
+  }, [])
+
+  const activityItems = activities.map((activity: any) => {
+    return (
+      <List.Item key={activity.id}>
+        {activity.title}
+      </List.Item>
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+      <List>
+        {activityItems}
+      </List>
     </div>
   );
 }
